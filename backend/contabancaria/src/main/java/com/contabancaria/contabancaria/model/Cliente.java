afe03fdd4bc088a9b91444049eb077759eef17d7
Gustavo Macedo <gustavomlima9393@gmail.com>
2023-02-13 18:50:52 -0300

    Adicionando Getters and Setters no Cliente

--- backend/contabancaria/src/main/java/com/contabancaria/contabancaria/model/Cliente.java
+++ backend/contabancaria/src/main/java/com/contabancaria/contabancaria/model/Cliente.java
@@ -1,7 +1,37 @@
 package com.contabancaria.contabancaria.model;
 
+import java.util.Date;
+
 public class Cliente {
 	
-	private Conta conta;
-
+	private String nome;
+	private Long cpf;
+	private Date dataNascimento;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
 }
