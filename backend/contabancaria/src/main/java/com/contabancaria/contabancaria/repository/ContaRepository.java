9b0a50171a545a5fc97570b1a93715c24b17bf7e
Gustavo Macedo <gustavomlima9393@gmail.com>
2023-02-13 13:55:57 -0300

    Terceiro commit teste

--- backend/contabancaria/src/main/java/com/contabancaria/contabancaria/repository/ContaRepository.java
+++ backend/contabancaria/src/main/java/com/contabancaria/contabancaria/repository/ContaRepository.java
@@ -1,5 +1,19 @@
 package com.contabancaria.contabancaria.repository;
 
-public class ContaRepository {
+/* public class ContaRepository {
 
+		// CRUD da Conta
+		public void procurarPorNumero(int numero);
+		public void listarTodas();
+		public void cadastrar(Conta conta);
+		public void atualizar(Conta conta);
+		public void deletar(int numero);
+		
+		// Métodos Bancários
+		public void depositar(int numero, float valor);
+		public void transferirPix(int numeroOrigem, int numeroDestino, float valor);
+		public void transferirTed(int numeroOrigem, int numeroDestino, float valor);
+		public void transferirDoc(int numeroOrigem, int numeroDestino, float valor);
+		
 }
+*/
